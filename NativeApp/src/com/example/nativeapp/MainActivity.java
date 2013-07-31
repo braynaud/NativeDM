478c7538b795debfa2977c79b2db81b9551edf48
jmc27 <jmc27@brandeis.edu>
2013-07-31 13:43:30 -0400

    switched out main activity for new version

--- NativeApp/src/com/example/nativeapp/MainActivity.java
+++ NativeApp/src/com/example/nativeapp/MainActivity.java
@@ -39,18 +39,32 @@ public class MainActivity extends Activity implements OnTouchListener {
          g.setOnTouchListener(this);
          View h = findViewById(R.id.Button08);
          h.setOnTouchListener(this);
+         View i = findViewById(R.id.kit1);
+         i.setOnTouchListener(this);
+         View j = findViewById(R.id.kit2);
+         j.setOnTouchListener(this);
+         View k = findViewById(R.id.kit3);
+         k.setOnTouchListener(this);
+         View l = findViewById(R.id.menu);
+         l.setOnTouchListener(this);
+         
          // Set the hardware buttons to control the music
-        // this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+         // this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
          // Load the sound
          soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-         soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
-             @Override
-             public void onLoadComplete(SoundPool soundPool, int sampleId,
-                     int status) {
-                 loaded = true;
-             }
-         });
-        soundID[0] = soundPool.load(this, R.raw.kick, 1);
+         load();
+    }
+ 
+    public void load()
+    {
+    	soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId,
+                    int status) {
+                loaded = true;
+            }
+        });
+    	soundID[0] = soundPool.load(this, R.raw.kick, 1);
         soundID[1] = soundPool.load(this, R.raw.snare, 1);
 	  	soundID[2] = soundPool.load(this, R.raw.clap, 1);
   		soundID[3] = soundPool.load(this, R.raw.cc, 1);
@@ -59,35 +73,80 @@ public class MainActivity extends Activity implements OnTouchListener {
   		soundID[6] = soundPool.load(this, R.raw.riffa, 1);
   		soundID[7] = soundPool.load(this, R.raw.riffd4, 1);
     }
- 
+    
+    public void play(int i, MotionEvent event)
+    {
+    	if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            // Getting the user sound settings
+            //AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            //float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            //float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            //float volume = actualVolume / maxVolume;
+            if (loaded) {
+                soundPool.play(soundID[i], 1, 1, 1, 0, 1f);
+            }
+        }
+    }
+    
     @Override
     public boolean onTouch(View v, MotionEvent event) {
     	if(v==findViewById(R.id.Button01))
+    	{
     		i=0;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button02))
+    	{
     		i=1;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button03))
+    	{
     		i=2;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button04))
+    	{
     		i=3;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button05))
+    	{
     		i=4;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button06))
+    	{
     		i=5;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button07))
+    	{
     		i=6;
+    		play(i,event);
+    	}
     	else if(v==findViewById(R.id.Button08))
+    	{
     		i=7;
-    	if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            // Getting the user sound settings
-            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-            float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-            float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-            float volume = actualVolume / maxVolume;
-            if (loaded) {
-                soundPool.play(soundID[i], volume, volume, 1, 0, 1f);
-            }
-        }
+    		play(i,event);
+    	}
+    	else if(v==findViewById(R.id.menu))
+    	{
+    		//switch to menu page
+    	}
+    	else if(v==findViewById(R.id.kit1))
+    	{
+    		
+    	}
+    	else if(v==findViewById(R.id.kit2))
+    	{
+    		
+    	}
+    	else if(v==findViewById(R.id.kit3))
+    	{
+    		
+    	}
+    	
         return false;
     }
 }
