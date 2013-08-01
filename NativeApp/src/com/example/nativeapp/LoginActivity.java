84f18523933486131d26618b5c6cd4982e6a8e67
jmc27 <jmc27@brandeis.edu>
2013-08-01 15:32:57 -0400

    updated login activity

--- NativeApp/src/com/example/nativeapp/LoginActivity.java
+++ NativeApp/src/com/example/nativeapp/LoginActivity.java
@@ -1,249 +1,391 @@
 package com.example.nativeapp;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
+import android.graphics.AvoidXfermode.Mode;
+import android.graphics.Color;
+import android.net.ParseException;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
 public class LoginActivity extends Activity {
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] {
-			"foo@example.com:hello", "bar@example.com:world" };
 
 	/**
-	 * The default email to populate the email field with.
-	 */
-	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+	  * Declare the PARSE CLIENT KEY and APPLICATION ID for the Parse Initialization
+	  */
+  protected String clientKey = "liTYaW6SCcVkoM9KFL03mQsQtv8nKq96umPv54DP";
+  protected String applicationId = "WZDdgyCHIW9EB3qh8uOyhh0ZeKkJOMv3j7DJVcW6";
 
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private UserLoginTask mAuthTask = null;
+  /**
+   * Twitter app consumer key
+   */
+  protected String twitterConsumerKey="qAFJzPXQlwAonT3aCSPgg";
 
-	// Values for email and password at the time of the login attempt.
-	private String mEmail;
-	private String mPassword;
+  /**
+   * Twitter app consumer secret
+   */
+  protected String twitterConsumerSecret = "M45UL6rX3KHYjZgcSupqREeMU5VFcYfnrHZl1mtGw";
+  
+  /**
+   * Facebook app consumer key 
+   */
+ 
+  protected String facebookConsumerSecret;
+  
+  /**
+   * Facebook app consumer secret
+   */
+  
+  protected String facebookConsumerKey;
+  
+  /**
+   * This is the onCreate method
+   * @param applicationId, clientID are the application id and client id for the parse initialization 
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-	// UI references.
-	private EditText mEmailView;
-	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.d("tag","login running");
-		final Activity loginActivityClass = this;
-		setContentView(R.layout.activity_login);
+    Parse.initialize(this, applicationId, clientKey);
+    ParseTwitterUtils.initialize(twitterConsumerKey, twitterConsumerSecret);
+    //ParseFacebookUtils.initialize(applicationId);
+    
+    setContentView(R.layout.main);
+    ((Button) findViewById(R.id.login)).getBackground().setColorFilter(Color.parseColor("#989898"), Mode.SRC_ATOP);
+    ((Button) findViewById(R.id.facebook)).getBackground().setColorFilter(Color.parseColor("#424de2"), Mode.SRC_IN);
+    ((Button) findViewById(R.id.twitter)).getBackground().setColorFilter(Color.parseColor("#5ca6ce"), Mode.SRC_ATOP);
+    ((Button) findViewById(R.id.signupbutton)).getBackground().setColorFilter(Color.parseColor("#989898"), Mode.SRC_ATOP);
+    
+    
+  }
 
-		// Set up the login form.
-		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
-		mEmailView = (EditText) findViewById(R.id.email);
-		mEmailView.setText(mEmail);
+  /**
+   * This is the TWITTER LOGIN CALLBACK
+   * @param twitterConsumerKey, twitterConsumerSecret 
+   */
 
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
-					}
-				});
+  public void twitterCallback(View v) {
+	  final ProgressDialog loginDialog = ProgressDialog.show(this, "",
+		        "Logging in. Please wait ...", true);
+    ParseTwitterUtils.logIn(this, new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException err) {
+    	  loginDialog.dismiss();
+        if (user == null) {
+        } else if (user.isNew()) {
+        	signedUpSuccessAlert();
+        	//After someone has been signed up, link their accounts to parse using the 
+        	//TwitterUtils.link to make sure they are linked to their account
+        	linkTwitterUser();
+        	
+        } else {
+        	
+          loginSuccess();
+ 
+        }
+      }
+      
+    });
+    
+  }
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
 
-		findViewById(R.id.sign_in_button).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						attemptLogin();
-						Log.d("tag","before intent");
-						Intent mainIntent = new Intent(loginActivityClass, MenuActivity.class);
-						startActivity(mainIntent);
-					}
-				});
-		
+  /**
+   * This is the FACEBOOK LOGIN CALLBACK
+   * @param facebookConsumerKey, facebookConsumerSecret
+   */
+
+ /* public void facebookCallback(View v) {
+   //Get the users inputed USERNAME and PASSWORD from the login page 
+    String username = getLoginUserName();
+    String password = getLoginPwd();
+    
+    //Verify that the usernames and password are valid before you register the user
+	  if (username.length() < 1) {
+		  	invalidUsernameAlert();
+		    dialog.dismiss();
+		    return;
+	  }
+	  if (password.length() <1){
+		  	invalidPasswordAlert();
+		    dialog.dismiss();
+		    return;
+	  }
+    ParseTwitterUtils.logIn(this, new LogInCallback() {
+      @Override
+      public void done(ParseUser user, ParseException err) {
+        if (user == null) {
+          listener.onError("facebook", err);
+        } else if (user.isNew()) {
+          listener.onSignup("facebook", user);
+        } else {
+          listener.onSignin("facebook", user);
+        }
+      }
+    });
+  }*/
+  
+  
+  /**
+   * Use getters to retrieve the usernames and passwords
+   * getLoginUserName gets the name of the user
+   * getLoginPwd gets the user's password 
+   */
+
+  private String getLoginUserName() {
+    return ((EditText) findViewById(R.id.username)).getText().toString();
+  }
+
+  private String getLoginPwd() {
+    return ((EditText) findViewById(R.id.pwd)).getText().toString();
+  }
+  /**
+   * LOGIN directly without using facebook or twitter
+   * @param View v
+   */
+
+  public void loginCallback(View v) {
+    final ProgressDialog dialog = ProgressDialog.show(this, "",
+        "Logging in ...", true);
+
+    //Get the users inputed USERNAME and PASSWORD from the login page 
+    String username = getLoginUserName();
+    String password = getLoginPwd();
+    
+    //Verify that the user names and password are valid before you register the user
+	  if (username.length() < 1) {
+		  	invalidUsernameAlert();
+		    dialog.dismiss();
+		    return;
+	  }
+	  if (password.length() <1){
+		  	invalidPasswordAlert();
+		    dialog.dismiss();
+		    return;
+	  }
+    
+    ParseUser.logInInBackground(getLoginUserName(), getLoginPwd(), new LogInCallback() {
+
+
+      public void done(ParseUser user, ParseException e) {
+        dialog.dismiss();
+        if (user != null ) {
+          //listener.onSignin("native", user);
+        	
+        	Log.d("Message","Login SUCCESS");
+        	loginSuccessfulAlert();
+        	loginSuccess();
+        }
+         //Otherwise if the user is new and doesnt have an account
+         //Create an account directly to parse 
+          else {
+      		//Create a log in failed alert to show the user that login failed
+        	  loginFailedAlert();
+
+        	 Log.d("Message","Login unsuccessful");
+          }
+      }
+    });
+  }
+ public void linkTwitterUser(){
+	//Check and see if the user is linked to the account. If he or she is not linked, make the link
+	    final ParseUser user = ParseUser.getCurrentUser();
+	    if (user != null) {
+	    if (!ParseTwitterUtils.isLinked(user)) {
+	    	  ParseTwitterUtils.link(user, this, new SaveCallback() {
+	    	    @Override
+	    	    public void done(ParseException ex) {
+	    	      if (ParseTwitterUtils.isLinked(user)) {
+	    	        Log.d("MyApp", "Woohoo, user logged in with Twitter!");
+	    	      }
+	    	    }
+	    	  });
+	    	}
+	    }
+ }
+ public void signupCallback(View v) {
+	 final View view = v;
+	  //Display a sign up alert to notify the user that they are being signed up
+	  createSignUpAlert();
+	 //Create a ProcessDialog to show the user that they are being signed up
+
+	  final ProgressDialog signupDialog = ProgressDialog.show(this, "",
+	        "Signing you up. Please wait ...", true);
+	  //Make sure that the user doesn't input a null value as the user name
+	  //if they do, return an alert and do not complete the signup
+	  String username = getLoginUserName();
+	  String password = getLoginPwd();
+	  if (username.length() < 1) {
+		  	invalidUsernameAlert();
+		    signupDialog.dismiss();
+          return;
+	  }
+	  if (password.length() <1){
+		  	invalidPasswordAlert();
+		    signupDialog.dismiss();
+		   return;
+
+	  }
+	  ParseUser newUser = new ParseUser();
+  	  newUser.setUsername(getLoginUserName());
+  	  newUser.setPassword(getLoginPwd());
+  	  newUser.signUpInBackground(new SignUpCallback() {
+  		  public void done(ParseException e) {
+  			  //Dismiss the signupDialog 
+  			  signupDialog.dismiss();
+  		    if (e == null) {
+  		      // Signup successful!
+  		  	  Log.d("Message","Signed Up successfully");
+  			signedUpSuccessAlert();
+  			loginProgress();
+  	  	  	//Give the device sometime to update the info in parse.com. This may take a few seconds.
+  	  	  	//Use countdowntimer to do this
+  			final Handler handler = new Handler();
+  			handler.postDelayed(new Runnable() {
+  	        @Override
+  	        public void run() {
+  	            // Give the device 5 seconds to delay as we wait for the timer to update.
+  	        	//Once that is done, prompt the device to log in the user. 
+  	          loginCallback(view);
+
+
+  	        }
+  	    }, 10000);
+  		    } else {
+  		      // Sign up didn't succeed. 
+  		    signupUnsuccessful();
+
+  		    }
+  		  }
+  		  
+  		});
+  	  
+  }
+ public void loginProgress(){
+	 ProgressDialog dialog = ProgressDialog.show(this, "",
+ 	        "Logging in ..." +
+ 	        "This may take a few seconds as we update your information", true);
+ }
+private void signupUnsuccessful(){
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setMessage("SignUp failed. Try again or choose a different username...");
+	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+	    }
+	});   
+	// Create the AlertDialog object and return it
+	builder.create();
+	builder.show();
+
+}
+private void invalidPasswordAlert() {
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setMessage("Password cannot be blank. Choose a correct password");
+	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+	    }
+	});   
+	// Create the AlertDialog object and return it
+	builder.create();
+	builder.show();
+}
+
+private void invalidUsernameAlert() {
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setMessage("Username cannot be blank. Choose a correct username");
+	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+	    }
+	});   
+	// Create the AlertDialog object and return it
+	builder.create();
+	builder.show();
+}
+
+ private void signedUpSuccessAlert() {
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setMessage("Signed up successfully... ");
+	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+	    }
+	});   
+	// Create the AlertDialog object and return it
+	builder.create();
+	builder.show();
+}
+
+ private void createSignUpAlert() {
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setMessage("Click ok and Sign you up!");
+	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+	    }
+	});   
+	// Create the AlertDialog object and return it
+	builder.create();
+	builder.show();
+}
+/**
+ * This is a method that loads a new activity after successfully login in to the system
+ * 
+ */
+	public void loginSuccess(){
+
+	Intent loginSuccess = new Intent(this, MainActivity.class);
+	startActivity(loginSuccess);
+	finish();
+  }
+
+  
+  /**
+   * This is a dialog builder that will be used to send alerts to the user
+   * in case they have any issue during sign up or login
+   */
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+      // Use the Builder class for convenient dialog construction
+      final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int id) {
+                 }
+             });   
+      // Create the AlertDialog object and return it
+      return builder.create();
+  }
+
+	private void loginFailedAlert() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Log in failed! Try facebook or twitter or signup if you haven't!");
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int id) {
+		    }
+		});   
+		// Create the AlertDialog object and return it
+		builder.create();
+		builder.show();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
+	private void loginSuccessfulAlert() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Log in successful!");
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int id) {
+		    }
+		});   
+		// Create the AlertDialog object and return it
+		builder.create();
+		builder.show();
 	}
-
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		if (mAuthTask != null) {
-			return;
-		}
-
-		// Reset errors.
-		mEmailView.setError(null);
-		mPasswordView.setError(null);
-
-		// Store values at the time of the login attempt.
-		mEmail = mEmailView.getText().toString();
-		mPassword = mPasswordView.getText().toString();
-
-		boolean cancel = false;
-		View focusView = null;
-
-		// Check for a valid password.
-		if (TextUtils.isEmpty(mPassword)) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			focusView = mPasswordView;
-			cancel = true;
-		} else if (mPassword.length() < 4) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(mEmail)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
-			cancel = true;
-		} else if (!mEmail.contains("@")) {
-			mEmailView.setError(getString(R.string.error_invalid_email));
-			focusView = mEmailView;
-			cancel = true;
-		}
-
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView.requestFocus();
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-			showProgress(true);
-			mAuthTask = new UserLoginTask();
-			mAuthTask.execute((Void) null);
-		}
-	}
-
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
-
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
-
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
-
-	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
-	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			// TODO: attempt authentication against a network service.
-
-			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
-			}
-
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
-			}
-
-			// TODO: register the new account here.
-			return true;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-			mAuthTask = null;
-			showProgress(false);
-
-			if (success) {
-				finish();
-			} else {
-				mPasswordView
-						.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
-			}
-		}
-
-		@Override
-		protected void onCancelled() {
-			mAuthTask = null;
-			showProgress(false);
-		}
-	}
+ 
 }
