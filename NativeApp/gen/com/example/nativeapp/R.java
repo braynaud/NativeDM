65aa5a61fab1d10a803770db5cadb05724a49140
jmc27 <jmc27@brandeis.edu>
2013-08-01 15:37:13 -0400

    updated strings

--- NativeApp/gen/com/example/nativeapp/R.java
+++ NativeApp/gen/com/example/nativeapp/R.java
@@ -39,18 +39,18 @@ public final class R {
         public static final int button2=0x7f090017;
         public static final int button3=0x7f090016;
         public static final int button4=0x7f090018;
-        public static final int email=0x7f090005;
+        public static final int facebook=0x7f090007;
+        public static final int instructions=0x7f090002;
         public static final int kit1=0x7f090013;
         public static final int kit2=0x7f090012;
         public static final int kit3=0x7f090011;
-        public static final int login=0x7f090007;
-        public static final int login_form=0x7f090004;
-        public static final int login_status=0x7f090002;
-        public static final int login_status_message=0x7f090003;
+        public static final int login=0x7f090006;
         public static final int menu=0x7f090014;
-        public static final int password=0x7f090006;
-        public static final int sign_in_button=0x7f090008;
+        public static final int pwd=0x7f090004;
+        public static final int signupbutton=0x7f090005;
         public static final int textView1=0x7f090000;
+        public static final int twitter=0x7f090008;
+        public static final int username=0x7f090003;
     }
     public static final class layout {
         public static final int activity_browser=0x7f030000;
@@ -75,24 +75,32 @@ public final class R {
         public static final int snare=0x7f040007;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f06000a;
+        public static final int action_forgot_password=0x7f060012;
         public static final int action_settings=0x7f060001;
-        public static final int action_sign_in_register=0x7f060008;
-        public static final int action_sign_in_short=0x7f060009;
+        public static final int action_sign_in_register=0x7f060010;
+        public static final int action_sign_in_short=0x7f060011;
+        public static final int also_login=0x7f060008;
         public static final int app_name=0x7f060000;
-        public static final int error_field_required=0x7f06000f;
-        public static final int error_incorrect_password=0x7f06000e;
-        public static final int error_invalid_email=0x7f06000c;
-        public static final int error_invalid_password=0x7f06000d;
+        public static final int error_field_required=0x7f060017;
+        public static final int error_incorrect_password=0x7f060016;
+        public static final int error_invalid_email=0x7f060014;
+        public static final int error_invalid_password=0x7f060015;
+        public static final int facebook=0x7f060009;
         public static final int hello_world=0x7f060003;
-        public static final int login_progress_signing_in=0x7f06000b;
+        public static final int instructions=0x7f06000c;
+        public static final int login=0x7f060007;
+        public static final int login_progress_signing_in=0x7f060013;
+        public static final int password=0x7f060006;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f060006;
-        public static final int prompt_password=0x7f060007;
+        public static final int prompt_email=0x7f06000e;
+        public static final int prompt_password=0x7f06000f;
+        public static final int signup=0x7f06000a;
         public static final int title_activity_browser=0x7f060004;
-        public static final int title_activity_login=0x7f060005;
+        public static final int title_activity_login=0x7f06000d;
         public static final int title_activity_menu=0x7f060002;
+        public static final int twitter=0x7f06000b;
+        public static final int username=0x7f060005;
     }
     public static final class style {
         /** 
