2382b8e5344b9f038b5149e0ed47ada29a6e1655
braynaud <braynaud@brandeis.edu>
2013-08-02 12:05:31 -0700

    sequencer kind of works, no pause button yet

--- NativeApp/src/com/example/nativeapp/SequencerActivity.java
+++ NativeApp/src/com/example/nativeapp/SequencerActivity.java
@@ -1,15 +1,19 @@
 package com.example.nativeapp;
 
+import java.util.*;
+
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.*;
 
 
 public class SequencerActivity extends Activity implements OnTouchListener {
@@ -18,7 +22,18 @@ public class SequencerActivity extends Activity implements OnTouchListener {
     private int[] soundID=new int[8];
     boolean loaded = false;
     private int i=0;
+    private Timer timer; 
+    private View padsel;
     private boolean[] timeDivs=new boolean[16];
+    private boolean[] SoundOne=new boolean[16];
+    private boolean[] SoundTwo=new boolean[16];
+    private boolean[] SoundThree=new boolean[16];
+    private boolean[] SoundFour=new boolean[16];
+    private boolean[] SoundFive=new boolean[16];
+    private boolean[] SoundSix=new boolean[16];
+    private boolean[] SoundSeven=new boolean[16];
+    private boolean[] SoundEight=new boolean[16];
+   
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +97,29 @@ public class SequencerActivity extends Activity implements OnTouchListener {
         t15.setOnTouchListener(this);
         View t16 = findViewById(R.id.timeDiv16);
         t16.setOnTouchListener(this);
+        SeekBar seekBpm = (SeekBar)findViewById(R.id.bpm); 
+        final TextView bpm = (TextView)findViewById(R.id.bpmText);
+        seekBpm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        { 
+        	   @Override 
+        	   public void onProgressChanged(SeekBar seekBpm, int progress, boolean fromUser) 
+        	   {
+        		progress+=60;
+        	    // TODO Auto-generated method stub 
+        	    bpm.setText(String.valueOf(progress)); 
+        	   } 
+
+        	   @Override 
+        	   public void onStartTrackingTouch(SeekBar seekBar) { 
+        	    // TODO Auto-generated method stub 
+        	   } 
+
+        	   @Override 
+        	   public void onStopTrackingTouch(SeekBar seekBar) { 
+        	    // TODO Auto-generated method stub 
+        	   } 
+        });
+
         
         // Set the hardware buttons to control the music
         // this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
@@ -109,7 +147,7 @@ public class SequencerActivity extends Activity implements OnTouchListener {
   		soundID[7] = soundPool.load(this, R.raw.riffd4, 1);
     }
 	
-	 public void play(int i, MotionEvent event)
+	 public void play(int i, View v, MotionEvent event)
 	    {
 	    	if (event.getAction() == MotionEvent.ACTION_DOWN) {
 	            // Getting the user sound settings
@@ -120,50 +158,56 @@ public class SequencerActivity extends Activity implements OnTouchListener {
 	            if (loaded) {
 	                soundPool.play(soundID[i], 1, 1, 1, 0, 1f);
 	            }
+	            ((Button)v).setBackgroundColor(Color.BLUE);
+	    		if(padsel!=null&&padsel!=v)
+	    			padsel.setBackgroundResource(android.R.drawable.btn_default);
+	            padsel=v;
+	            
 	        }
 	    }
 	    
 	    @Override
-	    public boolean onTouch(View v, MotionEvent event) {
+	    public boolean onTouch(View v, MotionEvent event) 
+	    {
 	    	if(v==findViewById(R.id.pad1))
 	    	{
 	    		i=0;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad2))
 	    	{
 	    		i=1;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad3))
 	    	{
 	    		i=2;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad4))
 	    	{
 	    		i=3;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad5))
 	    	{
 	    		i=4;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad6))
 	    	{
 	    		i=5;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad7))
 	    	{
 	    		i=6;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.pad8))
 	    	{
 	    		i=7;
-	    		play(i,event);
+	    		play(i,v,event);
 	    	}
 	    	else if(v==findViewById(R.id.menu))
 	    	{
@@ -182,48 +226,129 @@ public class SequencerActivity extends Activity implements OnTouchListener {
 	    	{
 	    		
 	    	}
+	    	else if(v==findViewById(R.id.playPause))
+	    	{
+	    			seqPlay();
+	    	}
 	    	else if(v==findViewById(R.id.timeDiv1))
-	    		timeDivs[0]=!timeDivs[0];
+	    		setPlay(0);
 	    	else if(v==findViewById(R.id.timeDiv2))
-	    		timeDivs[1]=!timeDivs[1];
+	    		setPlay(1);
 	    	else if(v==findViewById(R.id.timeDiv3))
-	    		timeDivs[2]=!timeDivs[2];
+	    		setPlay(2);
 	    	else if(v==findViewById(R.id.timeDiv4))
-	    		timeDivs[3]=!timeDivs[3];
+	    		setPlay(3);
 	    	else if(v==findViewById(R.id.timeDiv5))
-	    		timeDivs[4]=!timeDivs[4];
+	    		setPlay(4);
 	    	else if(v==findViewById(R.id.timeDiv6))
-	    		timeDivs[5]=!timeDivs[5];
+	    		setPlay(5);
 	    	else if(v==findViewById(R.id.timeDiv7))
-	    		timeDivs[6]=!timeDivs[6];
+	    		setPlay(6);
 	    	else if(v==findViewById(R.id.timeDiv8))
-	    		timeDivs[7]=!timeDivs[7];
+	    		setPlay(7);
 	    	else if(v==findViewById(R.id.timeDiv9))
-	    		timeDivs[8]=!timeDivs[8];
+	    		setPlay(8);
 	    	else if(v==findViewById(R.id.timeDiv10))
-	    		timeDivs[9]=!timeDivs[8];
+	    		setPlay(9);
 	    	else if(v==findViewById(R.id.timeDiv11))
-	    		timeDivs[10]=!timeDivs[10];
+	    		setPlay(10);
 	    	else if(v==findViewById(R.id.timeDiv12))
-	    		timeDivs[11]=!timeDivs[11];
+	    		setPlay(11);
 	    	else if(v==findViewById(R.id.timeDiv13))
-	    		timeDivs[12]=!timeDivs[12];
+	    		setPlay(12);
 	    	else if(v==findViewById(R.id.timeDiv14))
-	    		timeDivs[13]=!timeDivs[13];
+	    		setPlay(13);
 	    	else if(v==findViewById(R.id.timeDiv15))
-	    		timeDivs[14]=!timeDivs[14];
+	    		setPlay(14);
 	    	else if(v==findViewById(R.id.timeDiv16))
-	    		timeDivs[15]=!timeDivs[15];
-	    	
+	    		setPlay(15);
 	        return false;
 	    }
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sequencer, menu);
 		return true;
 	}
 	
-	
 
+	public void setPlay(int divPlay)
+	{
+
+		ToggleButton v;
+		
+    	v=(ToggleButton)findViewById(R.id.timeDiv2);
+    	v=(ToggleButton)findViewById(R.id.timeDiv3);
+    	v=(ToggleButton)findViewById(R.id.timeDiv4);
+    	v=(ToggleButton)findViewById(R.id.timeDiv5);
+    	v=(ToggleButton)findViewById(R.id.timeDiv6);
+    	v=(ToggleButton)findViewById(R.id.timeDiv7);
+    	v=(ToggleButton)findViewById(R.id.timeDiv8);
+    	v=(ToggleButton)findViewById(R.id.timeDiv9);
+    	v=(ToggleButton)findViewById(R.id.timeDiv10);
+    	v=(ToggleButton)findViewById(R.id.timeDiv11);
+    	v=(ToggleButton)findViewById(R.id.timeDiv12);
+    	v=(ToggleButton)findViewById(R.id.timeDiv13);
+    	v=(ToggleButton)findViewById(R.id.timeDiv14);
+    	v=(ToggleButton)findViewById(R.id.timeDiv15);
+    	v=(ToggleButton)findViewById(R.id.timeDiv16);
+		switch(i)
+		{
+			case 0: SoundOne[divPlay]=!SoundOne[divPlay];
+			v=(ToggleButton)findViewById(R.id.timeDiv1);
+			v.setChecked(true);
+			break;
+			case 1: SoundTwo[divPlay]=!SoundTwo[divPlay];
+			break;
+			case 2: SoundThree[divPlay]=!SoundThree[divPlay];
+			break;
+			case 3: SoundFour[divPlay]=!SoundFour[divPlay];
+			break;
+			case 4: SoundFive[divPlay]=!SoundFive[divPlay];
+			break;
+			case 5: SoundSix[divPlay]=!SoundSix[divPlay];
+			break;
+			case 6: SoundSeven[divPlay]=!SoundSeven[divPlay];
+			break;
+			case 7: SoundEight[divPlay]=!SoundEight[divPlay];
+			break;
+		}
+	}
+	
+	public void seqPlay()
+	{
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() 
+		{
+			  @Override
+			  public void run() 
+			  {
+			    int j=0;
+			    if(SoundOne[j])
+			    	soundPool.play(soundID[0], 1, 1, 1, 0, 1f);
+			    if(SoundTwo[j])
+			    	soundPool.play(soundID[1], 1, 1, 1, 0, 1f);
+			    if(SoundThree[j])
+			    	soundPool.play(soundID[2], 1, 1, 1, 0, 1f);
+			    if(SoundFour[j])
+			    	soundPool.play(soundID[3], 1, 1, 1, 0, 1f);
+			    if(SoundFive[j])
+			    	soundPool.play(soundID[4], 1, 1, 1, 0, 1f);
+			    if(SoundSix[j])
+			    	soundPool.play(soundID[5], 1, 1, 1, 0, 1f);
+			    if(SoundSeven[j])
+			    	soundPool.play(soundID[6], 1, 1, 1, 0, 1f);
+			    if(SoundEight[j])
+			    	soundPool.play(soundID[7], 1, 1, 1, 0, 1f);
+			    ++j;
+			    if(j==16)
+			    	j=0;
+			    timer.cancel();
+			  }
+		}, 0, 1000);
+		
+	}
+	
 }
+	
